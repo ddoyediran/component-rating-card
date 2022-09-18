45e3a3599d65a2dca242e677496350c53b42a224
ddoyediran <ddoyediran@gmail.com>
2022-09-18 18:22:55 +0200

    more commit

--- js/app.js
+++ js/app.js
@@ -3,4 +3,8 @@ let rating_section = document.querySelector(".rating-container");
 rating_section.addEventListener("click", function (event) {
   console.log("This was clicked!");
   console.log(rating_section.children);
+
+  let elementIdName = event.target.id;
+  //document.getElementById("elementIdName").style.backgroundColor = "red";
+  console.log(event.target);
 });
