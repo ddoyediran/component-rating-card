b66be4c4aa77b742d617cde6378469c73fec7403
ddoyediran <ddoyediran@gmail.com>
2022-09-18 18:20:43 +0200

    adding JS

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let rating_section = document.querySelector(".rating-container");
+
+rating_section.addEventListener("click", function (event) {
+  console.log("This was clicked!");
+  console.log(rating_section.children);
+});
